e66eb2b5955f1199198b725353ff7d88353a2911
Sodiq Alabi <adisco4420@gmail.com>
2023-09-20 03:57:04 +0200

    Implement save and delete favourite asset using localstorage.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,23 +11,39 @@ export class HomeComponent implements OnInit {
   assets: IAsset[] | undefined;
   pagination = { itemsPerPage: 20, currentPage: 1 };
   unFilteredAssets: IAsset[] | undefined;
-  searchTerm = ''
+  searchTerm = '';
+  favouriteAssets: string[] = [];
 
   constructor(private assetSrv: AssetService) { }
 
   ngOnInit(): void {
     this.fetchAssets();
+    this.fetchFavouriteAssets();
   }
 
   fetchAssets() {
-    this.assetSrv.fetchCryptoAssets().subscribe(assets => {
+    this.assetSrv.fetchAssets().subscribe(assets => {
       this.unFilteredAssets = this.assets = assets;
     })
+  }
+  isFavouriteAsset(asset: IAsset) {
+    return this.favouriteAssets.indexOf(asset.asset_id) >= 0;
+  }
+  fetchFavouriteAssets() {
+    this.favouriteAssets = this.assetSrv.fetchFavouriteAssets();
   }
   searchAsset() {
     this.pagination.currentPage = 1;
     const searchTerm = this.searchTerm.toLowerCase();
     this.assets = this.unFilteredAssets?.filter(asset => asset.name.toLowerCase().includes(searchTerm))    
+  }
+  saveFavouriteAsset(asset: IAsset) {
+    this.assetSrv.saveFavouriteAsset(asset);
+    this.fetchFavouriteAssets();
+  }
+  deleteFavouriteAsset(asset: IAsset) {
+    this.assetSrv.deleteFavouriteAsset(asset)
+    this.fetchFavouriteAssets();
   }
   pageChange(event: number) {
     this.pagination.currentPage = event;
--- src/app/services/asset.service.ts
+++ src/app/services/asset.service.ts
@@ -10,15 +10,33 @@ import { IAsset } from '../interfaces/asset.interface';
 export class AssetService {
   private base_url = environment.COIN_API_BASE_URL;
   private api_key = environment.COIN_API_KEY;
+  private localStorageKey = 'Favourite-Assets'
 
 
   constructor(private http: HttpClient) { }
 
-  fetchCryptoAssets(): Observable<IAsset[]> {
+  fetchAssets(): Observable<IAsset[]> {
     const headers = {'X-CoinAPI-Key': this.api_key}
     return this.http.get<IAsset[]>(`${this.base_url}/assets`, {headers}).pipe(map(assets => {
       return assets.filter(asset => asset.type_is_crypto)
     }))
   }
-
+  saveFavouriteAsset(asset: IAsset) {
+    const assets = this.fetchFavouriteAssets();
+    const result = JSON.stringify([...assets, asset.asset_id]);
+    localStorage.setItem(this.localStorageKey, result);
+  }
+  deleteFavouriteAsset(asset: IAsset) {
+    const assetIds = this.fetchFavouriteAssets();
+    const result = assetIds.filter(assetId => assetId !== asset.asset_id);
+    localStorage.setItem(this.localStorageKey, JSON.stringify(result));
+  }
+  fetchFavouriteAssets(): string[]  {
+    const assetIds = localStorage.getItem(this.localStorageKey);
+    let result: any  = [];
+    if(assetIds) {
+      result = JSON.parse(assetIds)
+    }
+    return result
+  }
 }
